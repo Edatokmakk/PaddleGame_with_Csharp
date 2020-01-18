0882eec12050842a2de89f8b7cbe0483007f4d5a
edato <edatokmakk@gmail.com>
2020-01-18 04:05:01 +0300

    c#

---
+++ Oyun_Projesi/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Oyun_Projesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.oyunalani = new System.Windows.Forms.Panel();
+            this.top = new System.Windows.Forms.PictureBox();
+            this.raket = new System.Windows.Forms.PictureBox();
+            this.time_lbl = new System.Windows.Forms.Label();
+            this.skor_button = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.oyunalani.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.top)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.raket)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // oyunalani
+            // 
+            this.oyunalani.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.oyunalani.Controls.Add(this.top);
+            this.oyunalani.Controls.Add(this.raket);
+            this.oyunalani.Location = new System.Drawing.Point(0, 56);
+            this.oyunalani.Name = "oyunalani";
+            this.oyunalani.Size = new System.Drawing.Size(1218, 635);
+            this.oyunalani.TabIndex = 2;
+            this.oyunalani.Paint += new System.Windows.Forms.PaintEventHandler(this.oyunalani_Paint_1);
+            this.oyunalani.MouseClick += new System.Windows.Forms.MouseEventHandler(this.oyunalani_MouseClick);
+            // 
+            // top
+            // 
+            this.top.BackColor = System.Drawing.Color.Olive;
+            this.top.Location = new System.Drawing.Point(597, 257);
+            this.top.Name = "top";
+            this.top.Size = new System.Drawing.Size(20, 20);
+            this.top.TabIndex = 1;
+            this.top.TabStop = false;
+            // 
+            // raket
+            // 
+            this.raket.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.raket.BackColor = System.Drawing.Color.SlateBlue;
+            this.raket.Location = new System.Drawing.Point(485, 605);
+            this.raket.Name = "raket";
+            this.raket.Size = new System.Drawing.Size(240, 20);
+            this.raket.TabIndex = 0;
+            this.raket.TabStop = false;
+            // 
+            // time_lbl
+            // 
+            this.time_lbl.AutoSize = true;
+            this.time_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.time_lbl.Location = new System.Drawing.Point(73, 22);
+            this.time_lbl.Name = "time_lbl";
+            this.time_lbl.Size = new System.Drawing.Size(21, 24);
+            this.time_lbl.TabIndex = 3;
+            this.time_lbl.Text = "0";
+            // 
+            // skor_button
+            // 
+            this.skor_button.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.skor_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.skor_button.Location = new System.Drawing.Point(312, 12);
+            this.skor_button.Name = "skor_button";
+            this.skor_button.Size = new System.Drawing.Size(221, 35);
+            this.skor_button.TabIndex = 2;
+            this.skor_button.Text = "YÜKSEK SKORLAR";
+            this.skor_button.UseVisualStyleBackColor = false;
+            this.skor_button.Click += new System.EventHandler(this.skor_button_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1218, 692);
+            this.Controls.Add(this.time_lbl);
+            this.Controls.Add(this.oyunalani);
+            this.Controls.Add(this.skor_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.oyunalani.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.top)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.raket)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel oyunalani;
+        private System.Windows.Forms.PictureBox top;
+        private System.Windows.Forms.PictureBox raket;
+        private System.Windows.Forms.Button skor_button;
+        private System.Windows.Forms.Label time_lbl;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Oyun_Projesi/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Media;
+using System.Timers;
+
+namespace Oyun_Projesi
+{
+    public partial class Form1 : Form
+    {
+        string project_dir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;//Bulunan proje dosyasının yolunu verir.
+        
+        public int speed_left = 3;//topun hızı
+        public int speed_top = 3;
+        public int skorlar = 0;
+        public DateTime then = DateTime.Now;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Enabled = false;
+            timer2.Enabled = true;
+            
+            this.TopMost = true;//Formu öne getir.
+            raket.Top = oyunalani.Bottom - (oyunalani.Bottom / 10);//raketin pozisyonunu kuruldu.
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+         
+            //this.Bounds = Screen.PrimaryScreen.Bounds;//tam ekran yapmak
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        { }
+
+        private void oyunalani_Paint(object sender, PaintEventArgs e)
+        { }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_MouseClick(object sender, MouseEventArgs e)//burada yeni oyun başlatacak kodu yazmak gerekli
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+            {
+            timer_2_and_1_same_jobs();
+            DateTime now = DateTime.Now;
+            time_lbl.Text = Convert.ToString((now - then));//geçen süreyi gösterir.
+
+            top.Left += speed_left; //topu harekete geçirme
+            top.Top += speed_top;
+
+            if (top.Bottom >= raket.Top && top.Bottom <= raket.Bottom && top.Left >= raket.Left && top.Right <= raket.Right) //raket çarpışması
+            {
+                speed_top += Convert.ToInt32((speed_top) * (0.1));
+                speed_left += Convert.ToInt32((speed_left) * (0.1));
+                speed_top = -speed_top; //yön değiştirme
+            }
+            if (top.Left <= oyunalani.Left)
+            {
+                speed_left = -speed_left;
+            }
+            if (top.Right >= oyunalani.Right)
+            {
+                speed_left = -speed_left;
+            }
+            if (top.Top <= oyunalani.Top)
+            {
+                speed_top = -speed_top;
+            }
+            if (top.Bottom >= oyunalani.Bottom)
+            {
+                timer1.Enabled = false;// top dışarıda.oyunu durdur
+                timer2.Enabled = true;
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(project_dir+"\\sad.wav");//proje dosyasının içinde bulunan sesi oynatmak için nesne tanımladık
+                player.Play();//Oyun bittiğinde gelen sesi oynatır.
+            }
+        }
+
+        private void skor_button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void oyunalani_MouseClick(object sender, MouseEventArgs e)
+        {
+            // oyun ilk başındaki haline döndürülüyor.
+            top.Top = 200;
+            top.Left = 500;
+            speed_left = 2; // oyun hızı başlangıca geri döndü
+            speed_top = 2;
+            skorlar = 0;
+            timer1.Enabled = true;
+            timer2.Enabled = false;
+            then = DateTime.Now;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            // oyun dururken hareketlerin gerçekleşmesi için timer2 oluşturduk.
+            timer_2_and_1_same_jobs();
+            
+        }
+
+        private void timer_2_and_1_same_jobs()
+        {
+
+            raket.Left = Cursor.Position.X - (raket.Width / 2); //raketin ortası imlecin pozisyonuna ayarlandı.
+            if (raket.Left + raket.Width > oyunalani.Right)
+            {
+                raket.Left = oyunalani.Right - raket.Width;
+            }
+            if (raket.Left < oyunalani.Left)
+            {
+                raket.Left = oyunalani.Left;
+            }
+            oyunalani.Anchor = AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Bottom;
+            //İmleci gizle. Mauseun oyun alanının içinde olması koşuluyla.
+            if (Cursor.Position.X <= oyunalani.Right && Cursor.Position.X >= oyunalani.Left)
+            {
+                if (Cursor.Position.Y >= oyunalani.Top && Cursor.Position.Y <= oyunalani.Bottom)
+                {
+                    Cursor.Hide(); 
+                }
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void oyunalani_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
+   
